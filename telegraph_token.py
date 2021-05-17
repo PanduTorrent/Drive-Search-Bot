8520c070bb52b44e6ac7f3f6c0ee64faf28cded5
Faris <81860288+FREAK-HEAD@users.noreply.github.com>
2021-05-17 17:20:28 +0530

    Update telegraph_token.py

--- telegraph_token.py
+++ telegraph_token.py
@@ -1,6 +1,6 @@
 from telegraph import Telegraph
 
 telegraph = Telegraph()
-telegraph.create_account(short_name=input("Enter a username for your Telegra.ph : "))
+telegraph.create_account(short_name=input("Enter a username for your Telegra.ph :FREAKHEAD"))
 
-print(f"Your Telegra.ph token ==>  {telegraph.get_access_token()}")
+print(f"Your Telegra.ph token ==>  {telegraph.get_access_token(10679e9976d1451a1650fe45197df8d4ae166037fd17e9f828d14c272750)}")
